a4664f9c2c7df9b542810830c6a7cf8e7a68c69a
sohomechowdhury <70840466+sohomechowdhury@users.noreply.github.com>
2021-08-01 00:50:24 +0530

    Update script.js

--- project2/script.js
+++ project2/script.js
@@ -1,11 +1,12 @@
-let taskbar=document.getElementsByClassName("taskbar")[0]
-let startmenu=document.getElementsByClassName("startmenu")[0]
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
 
-taskbar.addEventListner("click",( )=>{
-    if(startmenu.style.bottom =="50px"){
-        startmenu.style.bottom ="-655px"
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
     }
     else{
-        startmenu.style.bottom ="50px"
+        startmenu.style.bottom = "50px"
     }
 })
